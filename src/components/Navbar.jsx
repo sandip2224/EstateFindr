ea9dfcaf9bab710c291beb4d418a3b2d2baf8c55
sandip2224 <sandipan2224@gmail.com>
2023-04-20 20:52:40 +0530

    Navbar component setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,58 @@
+import { useNavigate, useLocation } from 'react-router-dom';
+import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg';
+import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg';
+import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg';
+
+function Navbar() {
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	const pathMatchRoute = (route) => {
+		return route === location.pathname;
+	};
+
+	const matchRouteColor = (route) => {
+		return pathMatchRoute(route) ? '#2c2c2c' : '#8f8f8f';
+	};
+
+	const matchRouteTextColor = (route) => {
+		return pathMatchRoute(route)
+			? 'navbarListItemNameActive'
+			: 'navbarListItemName';
+	};
+
+	return (
+		<footer className='navbar'>
+			<nav className='navbarNav'>
+				<ul className='navbarListItems'>
+					<li className='navbarListItem' onClick={() => navigate('/')}>
+						<ExploreIcon
+							fill={matchRouteColor('/')}
+							width='36px'
+							height='36px'
+						/>
+						<p className={matchRouteTextColor('/')}>Explore</p>
+					</li>
+					<li className='navbarListItem' onClick={() => navigate('/offers')}>
+						<OfferIcon
+							fill={matchRouteColor('/offers')}
+							width='36px'
+							height='36px'
+						/>
+						<p className={matchRouteTextColor('/offers')}>Offers</p>
+					</li>
+					<li className='navbarListItem' onClick={() => navigate('/profile')}>
+						<PersonOutlineIcon
+							fill={matchRouteColor('/profile')}
+							width='36px'
+							height='36px'
+						/>
+						<p className={matchRouteTextColor('/profile')}>Profile</p>
+					</li>
+				</ul>
+			</nav>
+		</footer>
+	);
+}
+
+export default Navbar;
