98e29991d381a39af2e6198c2bfe2c3c0042b875
sandip2224 <sandipan2224@gmail.com>
2023-04-21 21:16:08 +0530

    Profile page with logged in user

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { getAuth } from 'firebase/auth';
 
 function Profile() {
-	return (
-		<>
-			<h1>Profile</h1>
-		</>
-	);
+	const [user, setUser] = useState(null);
+	const auth = getAuth();
+	useEffect(() => {
+		setUser(auth.currentUser);
+	}, []);
+
+	return user ? <h1>Hello {user.displayName}</h1> : <h1>'Not logged in!'</h1>;
 }
 
 export default Profile;
