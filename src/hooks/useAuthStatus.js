3d08a230b7c53ed54053d48438b025c9b8c49a6f
sandip2224 <sandipan2224@gmail.com>
2023-04-23 12:42:35 +0530

    Minor refactoring

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 export const useAuthStatus = () => {
