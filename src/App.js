f3b3c299c006a5d4d9ecaed7f45cd37f19ed7b22
sandip2224 <sandipan2224@gmail.com>
2023-04-25 21:52:57 +0530

    React routing for create-listing

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Category from './pages/Category'
+import CreateListing from './pages/CreateListing'
+
 import PrivateRoute from './components/PrivateRoute'
 import ForgotPassword from './pages/ForgotPassword'
 
@@ -19,16 +21,17 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<Explore />}></Route>
-          <Route path='/offers' element={<Offers />}></Route>
-          <Route path='/category/:categoryName' element={<Category />}></Route>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/category/:categoryName' element={<Category />} />
           {/* Protected Route */}
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
           </Route>
-          <Route path='/sign-in' element={<SignIn />}></Route>
-          <Route path='/sign-up' element={<SignUp />}></Route>
-          <Route path='/forgot-password' element={<ForgotPassword />}></Route>
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/create-listing' element={<CreateListing />} />
         </Routes>
         <Navbar />
       </Router>
