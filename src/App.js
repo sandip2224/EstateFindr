ebbc41dfd63750d846538815ba1797c2e849f467
sandip2224 <sandipan2224@gmail.com>
2023-04-26 11:40:28 +0530

    Routing config

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Category from './pages/Category'
 import CreateListing from './pages/CreateListing'
+import Listing from './pages/Listing'
 
 import PrivateRoute from './components/PrivateRoute'
 import ForgotPassword from './pages/ForgotPassword'
@@ -32,6 +33,7 @@ function App() {
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route path='/category/:categoryName/:listingId' element={<Listing />} />
         </Routes>
         <Navbar />
       </Router>
